c83da61d7fefc1b25e86f9e57e2326b2d0ff79be
ikimiti4408 <91766187+ikimiti4408@users.noreply.github.com>
2022-06-05 03:26:01 +0900

    Ver0.20a
    make help
    UI upgrade

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-function change_mode(){
+//basic_data
+const ver = "0.1.1a";
+const APP_URL = "https://ikimiti4408.github.io/noumm/"
+const OFFLINE_URL = "https://github.com/ikimiti4408/noumm/tree/main/offline"
+let online = true;
+if (location.protocol == 'file:') online = false;
+
+function addAttribute(source, qualifiedName, add_text) {
+    let value = source.getAttribute(qualifiedName);
+    value += add_text;
+    source.setAttribute(qualifiedName, value);
+}
+
+function change_mode() {
     var b = document.getElementById("b");
     var c = document.getElementById("c");
-    //const b = document.querySelector("#b");
-    if(b.paused){
+    if (b.paused) {
         b.play();
         c.textContent = "ON";
         c.style.background = 'lightgreen';
         c.style.color = 'black';
-    }else{
+    } else {
         b.pause();
         c.textContent = "OFF";
         c.style.background = 'gray';
         c.style.color = 'white';
     }
 }
+
+function ver_write() {
+    const vec = document.getElementsByClassName("ver");
+    for (let i = 0; i < vec.length; i++) {
+        vec[i].textContent = ver;
+    }
+}
+
+function online_check() {
+    const SOURCE = document.getElementsByClassName("online")[0];
+    let url, text;
+    if (online) {
+        url = OFFLINE_URL;
+        text = "オフライン版"
+    } else {
+        url = APP_URL;
+        text = "オンライン版"
+    }
+    SOURCE.setAttribute("href", url);
+    SOURCE.textContent = text;
+}
+
+window.onload = function () {
+    ver_write();
+    online_check();
+};
